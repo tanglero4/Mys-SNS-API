94dd05b305d3185f82df88a20c6120e251bb2837
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2023-02-27 19:39:32 -0500

    Added user controller

--- Main/config/connecting.js
+++ Main/config/connecting.js
@@ -1,9 +1,9 @@
-const { connect, connection } = require('mongoose');
+const { connecting, connection } = require('mongoose');
 
 const connectingString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/snsDB';
 
-connect(connectingString, {
+connecting(connectingString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
